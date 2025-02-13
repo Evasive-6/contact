34cfda24423a90c956e24c3368481bc8f5d02ae9
Evasive-6 <albinshiju285@gmail.com>
2025-02-13 10:05:01 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0', // Bind to 0.0.0.0
-    port: 3000 // You can specify any port here
+    port: 3000, // You can specify any port here
+    allowedHosts: ['contact-1-hcf4.onrender.com'], // Add your host here
   }
 });
